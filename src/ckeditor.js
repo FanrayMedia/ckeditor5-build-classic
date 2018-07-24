6950115a8cd826fc976fc4402f1b1c15258aba32
Ray Fan <FanrayMedia@users.noreply.github.com>
2018-07-24 16:07:02 -0700

    fix: Editor headings output wrong `h tags` #7

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -94,6 +94,16 @@ ClassicEditor.defaultConfig = {
 			'redo',
 		]
 	},
+	// fix https://github.com/FanrayMedia/ckeditor5-build-classic/issues/7
+	heading: {
+		options: [
+			{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+			{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+			{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+			{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+			{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+		]
+	},
 	// https://docs.ckeditor.com/ckeditor5/latest/features/image.html#configuring-image-styles
 	image: {
 		// You need to configure the image toolbar, too, so it uses the new style buttons.

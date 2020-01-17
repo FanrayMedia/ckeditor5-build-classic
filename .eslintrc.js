9e6222a561a5f0b3e43f66c8e7ab11ce15cc7ea5
Ray Fan <FanrayMedia@users.noreply.github.com>
2020-01-16 16:25:18 -0800

    Upgrade to v16

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 /**
  * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 /* eslint-env node */
@@ -8,5 +8,8 @@
 'use strict';
 
 module.exports = {
-	extends: 'ckeditor5'
+	extends: 'ckeditor5',
+	rules: {
+		"linebreak-style": 0
+	}
 };
--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,25 +1,24 @@
 /**
  * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 // The editor creator to use.
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-// import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
@@ -28,14 +27,15 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
-import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import Imagr from '../plugins/ckeditor5-imagr/src/imagr';
-
 import './custom.css';
 
 export default class ClassicEditor extends ClassicEditorBase { }
@@ -43,39 +43,34 @@ export default class ClassicEditor extends ClassicEditorBase { }
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
-	// https://github.com/ckeditor/ckeditor5-autoformat/blob/master/src/autoformat.js
 	Autoformat,
 	Bold,
 	Italic,
 	Underline,
 	Strikethrough,
 	BlockQuote,
-	CKFinder,
 	EasyImage,
 	Heading,
 	Image,
 	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
-	// ImageUpload,
+	ImageResize,
+	Indent,
 	Link,
 	List,
+	MediaEmbed,
 	Paragraph,
-	Autosave,
-	Alignment,
-	// https://docs.ckeditor.com/ckeditor5/latest/features/highlight.html
-	// https://docs.ckeditor.com/ckeditor5/latest/api/module_highlight_highlight-HighlightConfig.html#member-options
-	Highlight,
-	// https://github.com/ckeditor/ckeditor5-autoformat/blob/master/docs/_snippets/features/autoformat.js
-	Code,
-	// https://docs.ckeditor.com/ckeditor5/latest/features/table.html
-
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	Imagr,
-	MediaEmbed
+	Autosave,
+	Alignment,
+	Code, // part of basic-styles
+	CodeBlock,
+	Highlight,
+	IndentBlock,
+	Imagr
 ];
 
 // Editor configuration.
@@ -89,17 +84,19 @@ ClassicEditor.defaultConfig = {
 			'strikethrough',
 			'bulletedList',
 			'numberedList',
-			'blockQuote',
-			'code',
 			'alignment',
+			'indent',
+			'outdent',
 			'highlight',
-			'link',
-			// 'imageUpload',
 			'imagr',
-			'mediaEmbed',
+			'blockQuote',
+			'link',
+			'code',
+			'codeBlock',
 			'insertTable',
+			'mediaEmbed',
 			'undo',
-			'redo',
+			'redo'
 		]
 	},
 	// fix https://github.com/FanrayMedia/ckeditor5-build-classic/issues/7
@@ -112,10 +109,15 @@ ClassicEditor.defaultConfig = {
 			{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
 		]
 	},
-	// https://docs.ckeditor.com/ckeditor5/latest/features/image.html#configuring-image-styles
 	image: {
 		// You need to configure the image toolbar, too, so it uses the new style buttons.
-		toolbar: ['imageTextAlternative', '|', 'imageStyle:alignLeft', 'imageStyle:full', 'imageStyle:alignRight'],
+		toolbar: [
+			'imageTextAlternative',
+			'|',
+			'imageStyle:alignLeft',
+			'imageStyle:full',
+			'imageStyle:alignRight'
+		],
 
 		styles: [
 			// This option is equal to a situation where no style is applied.
@@ -133,6 +135,23 @@ ClassicEditor.defaultConfig = {
 			'tableColumn',
 			'tableRow',
 			'mergeTableCells'
+		]
+	},
+	codeBlock: {
+		languages: [
+			{ language: 'plaintext', label: 'Plain text' }, // The default language.
+			{ language: 'cs', label: 'C#' },
+			{ language: 'xml', label: 'HTML/XML' },
+			{ language: 'css', label: 'CSS' },
+			{ language: 'javascript', label: 'JavaScript' },
+			{ language: 'typescript', label: 'TypeScript' },
+			{ language: 'diff', label: 'Diff' },
+			{ language: 'php', label: 'PHP' },
+			{ language: 'python', label: 'Python' },
+			{ language: 'java', label: 'Java' },
+			{ language: 'cpp', label: 'C++' },
+			{ language: 'c', label: 'C' },
+			{ language: 'ruby', label: 'Ruby' },
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
